30845f5e4d916bcddcb32fd58133e7402ce5862e
archimedes1515 <43996223+archimedes1515@users.noreply.github.com>
2023-06-30 15:01:28 +0300

    Add files via upload

--- WB_task2_L6/feature_engineering.py
+++ WB_task2_L6/feature_engineering.py
@@ -5,24 +5,29 @@ import pandas as pd
 
 
 def avg_word_len(s: str) -> float:
+    """функция для расчета средней длины слова в отзыве"""
     return np.mean(list(map(len, word_tokenize(s, language='russian'))))
 
 
 def avg_sent_len(s: str) -> float:
+    """функция для расчета средней длины предложения в отзыве"""
     return np.mean(list(map(len, sent_tokenize(s, language='russian'))))
 
 
 def caps_portion(s: str, text_len: int) -> float:
+    """функция для расчета доли капса в отзыве"""
     l = re.findall('[А-ЯA-Z]+', s)
     return sum(map(len, l)) / text_len
 
 
 def punct_portion(s: str, text_len: int) -> float:
+    """функция для расчета доли пунктуации в отзыве"""
     l = re.findall(r'[!"#$%&\'()*+,-./:;<=>?@\\^_`{|}~]+', s)
     return sum(map(len, l)) / text_len
 
 
 def generate_features(df: pd.DataFrame) -> pd.DataFrame:
+    """функция для генерации дополнительных фичей"""
     df['text_len'] = df.text.str.len()
     df['word_count'] = df.text.apply(lambda x: len(word_tokenize(x, language='russian')))
     df['sent_count'] = df.text.apply(lambda x: len(sent_tokenize(x, language='russian')))
--- WB_task2_L6/inference.py
+++ WB_task2_L6/inference.py
@@ -9,15 +9,17 @@ from pymorphy2 import MorphAnalyzer
 import feature_engineering as fe
 
 
-m = MorphAnalyzer()
-stop = stopwords.words('russian') + list(punctuation)
+m = MorphAnalyzer()  # подгрузим лемматизатор из pymorphy
+stop = stopwords.words('russian') + list(punctuation)  # а также стоп-слова и знаки-пунктуации
 
 
 def lemmatize_word(token, pymorphy=m):
+    """функция, выполняющая лематизацию слова"""
     return pymorphy.parse(token)[0].normal_form
 
 
 def lemmatize_text(text):
+    """функция, выполняющая лематизацию целого текста"""
     return [lemmatize_word(w) for w in text]
 
 
@@ -33,28 +35,28 @@ def preprocess_text(text, stop_w=stop, tokenizer=word_tokenize):
 
 
 def full_preprocess(df, column_transformer, vectorizer):
-    df = fe.generate_features(df)
-    df['clear_text'] = df.text.apply(preprocess_text)
-    vectorized_text = vectorizer.transform(df.clear_text)
+    df = fe.generate_features(df)  # сгенерируем признаки
+    df['clear_text'] = df.text.apply(preprocess_text)  # выполняем подготовку текстов отзывов
+    vectorized_text = vectorizer.transform(df.clear_text)  # векторизация текстов
     text_df = pd.DataFrame(vectorized_text.toarray(),
                            columns=vectorizer.get_feature_names_out(),
                            index=df.index)
-    df = df.drop(columns=['id1', 'id2', 'id3', 'text', 'clear_text'])
+    df = df.drop(columns=['id1', 'id2', 'id3', 'text', 'clear_text'])  # удаляем лишние колонки
     X = pd.concat([df, text_df], axis=1)
-    return column_transformer.transform(X)
+    return column_transformer.transform(X)  # нормируем признаки
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('sample_data.csv')
+    df = pd.read_csv('sample_data.csv')  # считаем файлик с сэмплом из исходных данных
 
     with open('column_transformer.pkl', 'rb') as f1:
         column_transformer = pickle.load(f1)
-    with open('bow_vectorizer.pkl', 'rb') as f2:
+    with open('bow_vectorizer.pkl', 'rb') as f2:  # подгрузим готовый векторайзер текстов
         vectorizer = pickle.load(f2)
-    with open('knn_bow_cv1.pkl', 'rb') as f3:
+    with open('knn_bow_cv1.pkl', 'rb') as f3:  # подгрузим обученную модель
         model = pickle.load(f3)
 
-    X = full_preprocess(df, column_transformer, vectorizer)
-    preds = model.predict(X)
-    print(preds)
-    # np.save('predictions.npy', preds)
+    X = full_preprocess(df, column_transformer, vectorizer)  # выполним полную подготовку данных к инференсу
+    preds = model.predict(X)  # получим предсказания модели
+    print(preds)  # распечатаем предсказания модели
+    np.save('predictions.npy', preds)  # сохраним предсказания модели в файлик
